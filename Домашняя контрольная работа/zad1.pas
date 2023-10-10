f2e83ea12db9971ce3a1dc3a01ab7f5ad4ea025e
makarbel <144238241+makarbel@users.noreply.github.com>
2023-10-10 08:36:52 +0300

    Add files via upload

---
+++ Домашняя контрольная работа/zad1.pas
@@ -0,0 +1,15 @@
+﻿program zad1;
+var x,result:real;
+begin
+  write('Введите значение х: ');
+  readln(x);
+    if x < -6 then
+      result := power(x, 2) - power(x, 2)
+      else
+      if (x >= -6) and (x < -4) then
+      result := ln(x) + power(x, 0.1 * x)
+      else
+      if (x >= -4) then
+      result := (exp(x) / power(x, 3)) * (log10(x) / power(x, sqrt(x)));  
+  writeln('Значение функции: ', result);
+end.
---
+++ Домашняя контрольная работа/zad2.pas
@@ -0,0 +1,18 @@
+﻿program zad2;
+var x,result:real;
+begin
+  x := -8;
+  while x <= -2 do
+  begin
+    if x < -6 then
+      result := power(x, 2) - power(x, 2)
+      else
+      if (x >= -6) and (x < -4) then
+      result := ln(x) + power(x, 0.1 * x)
+      else
+      if (x >= -4) then
+      result := (exp(x) / power(x, 3)) * (log10(x) / power(x, sqrt(x)));
+    writeln('Значение функции при x = ', x, ': ', result);
+    x := x + 0.3;
+  end;
+end.
